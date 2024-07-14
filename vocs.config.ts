94fb70631dfebe2cca6291d71d20578fc894c47b
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-07-14 03:42:01 +0200

    evm actions doc wip

--- vocs.config.ts
+++ vocs.config.ts
@@ -4,12 +4,24 @@ export default defineConfig({
   title: 'EVM Actions',
   sidebar: [
     {
-      text: 'What are EVM Actions?',
-      link: '/what-are-evm-actions',
+      text: 'Getting started',
+      link: '/getting-started',
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: 'Action starter kit',
+      link: '/starter-kit',
+    },
+    {
+      text: 'Declaring Actions',
+      link: '/declaring-actions',
+    },
+    {
+      text: 'Action types',
+      link: '/action-types',
+    },
+    {
+      text: 'Shortcuts',
+      link: '/shortcuts',
     },
   ],
 })
